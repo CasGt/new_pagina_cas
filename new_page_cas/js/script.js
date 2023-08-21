a2506e9aaf39642c3db85a33290c2fb14c67781b
hesler <hlopez@cas.edu.gt>
2023-08-21 10:30:57 -0600

    icono wp, mejoras navbar

--- new_page_cas/js/script.js
+++ new_page_cas/js/script.js
@@ -1,26 +1,32 @@
-// search-box open close js code
-let navbar = document.querySelector(".navbar");
-
 // sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+let mainContentContainer = document.querySelector(".main-content"); // Selector del contenedor de contenido principal
+
 menuOpenBtn.onclick = function () {
     navLinks.style.left = "0";
+    document.body.style.overflow = "hidden"; // Oculta el desplazamiento del cuerpo principal
 }
 menuCloseBtn.onclick = function () {
     navLinks.style.left = "-100%";
+    document.body.style.overflow = "auto"; // Habilita el desplazamiento del cuerpo principal
 }
 
-
+// Restaura el desplazamiento del cuerpo principal cuando se cierra el menú
+navLinks.addEventListener("transitionend", function () {
+    if (navLinks.style.left === "-100%") {
+        document.body.style.overflow = "auto"; // Habilita el desplazamiento del cuerpo principal
+    }
+});
 // sidebar submenu open close js code
 let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.addEventListener("click", function(e) {
+htmlcssArrow.addEventListener("click", function (e) {
     e.preventDefault();
     navLinks.classList.toggle("show1");
-  });
+});
 
-  
+
 let moreArrow = document.querySelector(".more-arrow");
 moreArrow.onclick = function () {
     navLinks.classList.toggle("show2");
@@ -31,6 +37,6 @@ jsArrow.onclick = function () {
 }
 
 let othersArrow = document.querySelector(".others-arrow");
-othersArrow.onclick = function() {
-  navLinks.classList.toggle("show4");
+othersArrow.onclick = function () {
+    navLinks.classList.toggle("show4");
 }
